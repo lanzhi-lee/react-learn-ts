1450be86ff64063fcfcf91cb401957c88f7c2a04
Li Zhan <lizhan1@xiaomi.com>
2019-07-14 18:31:38 +0800

    基本框架搭建完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header'
+import List from './components/list'
+import Footer from './components/footer';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <List />
+      <Footer />
     </div>
   );
 }
