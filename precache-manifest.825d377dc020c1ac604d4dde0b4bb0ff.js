91972e9db9ddb5c3536d1f3f45378871b34ae972
baibai-lee <baibai-lee@users.noreply.github.com>
2020-03-14 06:30:02 +0000

    Automated deployment: Sat Mar 14 06:30:02 UTC 2020 30a228f943c582605457627252bcae2975b4e2d8

---
+++ precache-manifest.825d377dc020c1ac604d4dde0b4bb0ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b628ac3e56e82d72af2b7eddb068c067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aea2c858d0e20c6202c",
+    "url": "/static/css/main.80cfdc4e.chunk.css"
+  },
+  {
+    "revision": "ab11d268c393029d322f",
+    "url": "/static/js/2.7d16d779.chunk.js"
+  },
+  {
+    "revision": "1aea2c858d0e20c6202c",
+    "url": "/static/js/main.175fdd97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
